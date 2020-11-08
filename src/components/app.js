ffa6732069317b506f4157863ebeebb6ed1164b9
Adil Shaikh <adil@invideo.io>
2020-11-08 23:31:36 +0530

    feat: added router

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import Experience from "./experience";
 import AboutMe from "./about-me";
+import Router from "preact-router";
+import Literacy from "./literacy";
 
 const App = () => (
     <div id="app">
-        <AboutMe/>
+        <Router>
+            <AboutMe path="/about-me" />
+            <Experience path="/experience" />
+            <Literacy path="/literacy" />
+            <AboutMe default />
+        </Router>
     </div>
 )
 
