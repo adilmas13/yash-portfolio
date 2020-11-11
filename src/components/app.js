30d9d9975f0bfda2cec48cf5da308038d363414e
Adil Shaikh <adil@invideo.io>
2020-11-11 23:17:27 +0530

    feat: added scroll

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,36 @@
-import Experience from "./experience";
 import AboutMe from "./about-me";
-import Router from "preact-router";
+import style from './style.css';
+import Experience from "./experience";
 import Literacy from "./literacy";
+import {useEffect, useState} from "preact/hooks";
 
-const App = () => (
-    <div id="app">
-        <Router>
-            <AboutMe path="/about-me" />
-            <Experience path="/experience" />
-            <Literacy path="/literacy" />
-            <AboutMe default />
-        </Router>
+
+const App = () => {
+    const [pageNo, setPageNo] = useState(0);
+
+    useEffect(() => {
+        const element = document.getElementById("scroller");
+        element.scroll({
+            left : document.body.clientWidth * pageNo,
+            behavior : "smooth"
+        })
+        return () => {};
+    }, [pageNo]);
+
+    const onPrevClicked = () => setPageNo((currentPageNo) => currentPageNo - 1);
+    const onNextClicked = () => setPageNo((currentPageNo) => currentPageNo + 1);
+
+   return <div id="app">
+        <div class={style.wrapper}>
+            <div class={style["scroll-container"]} id="scroller">
+                <AboutMe/>
+                <Experience/>
+                <Literacy/>
+            </div>
+            {pageNo > 0 && <div class={style.left} onClick={onPrevClicked}>prev</div>}
+           {pageNo < 2 &&  <div class={style.right} onClick={onNextClicked}>next</div>}
+        </div>
     </div>
-)
+}
 
 export default App;
