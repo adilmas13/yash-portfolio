70aba462e5ddfb6a44cf649fc3c266dddde6e7de
Adil Shaikh <adil@invideo.io>
2020-11-21 22:08:30 +0530

    feat: added adverts as part of router

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,12 @@
-import AboutMe from "./about-me";
-import style from './style.css';
-import Experience from "./experience";
-import Literacy from "./literacy";
-import {useEffect, useState} from "preact/hooks";
-
+import Adverts from "./adverts";
+import Router from "preact-router";
+import Home from "./home";
 
 const App = () => {
-    const [pageNo, setPageNo] = useState(0);
-
-    useEffect(() => {
-        const element = document.getElementById("scroller");
-        element.scroll({
-            left : document.body.clientWidth * pageNo,
-            behavior : "smooth"
-        })
-        return () => {};
-    }, [pageNo]);
-
-    const onPrevClicked = () => setPageNo((currentPageNo) => currentPageNo - 1);
-    const onNextClicked = () => setPageNo((currentPageNo) => currentPageNo + 1);
-
-   return <div id="app">
-        <div class={style.wrapper}>
-            <div class={style["scroll-container"]} id="scroller">
-                <AboutMe/>
-                <Experience/>
-                <Literacy/>
-            </div>
-            {pageNo > 0 && <div class={style.left} onClick={onPrevClicked}>prev</div>}
-           {pageNo < 2 &&  <div class={style.right} onClick={onNextClicked}>next</div>}
-        </div>
-    </div>
+   return <Router>
+      <Home default path="/home" />
+      <Adverts path="/adverts" />
+   </Router>
 }
 
 export default App;
---
+++ src/components/home/index.js
@@ -0,0 +1,35 @@
+import style from './style.css';
+import {useEffect, useState} from "preact/hooks";
+import AboutMe from "../about-me";
+import Experience from "../experience";
+import Literacy from "../literacy";
+
+const Home = () => {
+    const [pageNo, setPageNo] = useState(0);
+
+    useEffect(() => {
+        const element = document.getElementById("scroller");
+        element.scroll({
+            left : document.body.clientWidth * pageNo,
+            behavior : "smooth"
+        })
+        return () => {};
+    }, [pageNo]);
+
+    const onPrevClicked = () => setPageNo((currentPageNo) => currentPageNo - 1);
+    const onNextClicked = () => setPageNo((currentPageNo) => currentPageNo + 1);
+
+    return <div id="app">
+        <div class={style.wrapper}>
+            <div class={style["scroll-container"]} id="scroller">
+                <AboutMe/>
+                <Experience/>
+                <Literacy/>
+            </div>
+            {pageNo > 0 && <div class={style.left} onClick={onPrevClicked}>prev</div>}
+            {pageNo < 2 &&  <div class={style.right} onClick={onNextClicked}>next</div>}
+        </div>
+    </div>
+}
+
+export default Home;
