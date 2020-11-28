9d4af6a2299464fe849795dab1fe7b3158ee1207
Adil Shaikh <adil@invideo.io>
2020-11-29 00:12:48 +0530

    feat: created loader component

---
+++ src/components/loader/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "preact";
+import style from './style.css';
+
+const Loader = () => {
+    return <div class={style.parent}>
+        Loading...
+    </div>
+};
+
+export default Loader;
