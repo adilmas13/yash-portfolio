a09ac70addabdb3434379e05528f8b183eecdd18
Adil Shaikh <adil@invideo.io>
2020-11-29 00:26:57 +0530

    feat: home page component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import Adverts from "./adverts";
 import Router from "preact-router";
 import AboutMe from "./about-me";
+import Home from "./home";
 
 const App = () => {
     return <Router>
+        <Home path="/home" />
         <AboutMe default path="/about-me" />
         <Adverts path="/adverts" />
     </Router>
---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "preact";
+import style from './style.css';
+
+const Home = () => {
+    return <div class={style.parent}>
+        Home page coming soon
+    </div>
+};
+
+export default Home;
