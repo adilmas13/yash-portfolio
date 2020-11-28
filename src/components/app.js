3966f88e0c1eaf6edf55671d7c1f55b96450449c
Adil Shaikh <adil@invideo.io>
2020-11-29 00:43:49 +0530

    feat: added loader for lazy loading route

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,32 @@
 import Router from "preact-router";
 import AsyncRoute from 'preact-async-route';
+import Loader from "./loader";
 
 const App = () =>
     <Router>
         <AsyncRoute
             path="/home"
             getComponent={() => import('./home/index').then(module => module.default)}
-            loading={() => <div>loading...</div>}
+            loading={() => <Loader />}
         />
         <AsyncRoute
             default
             path="/about-me"
             getComponent={() => import('./about-me/index').then(module => module.default)}
-            loading={() => <div>loading...</div>}
+            loading={() => <Loader />}
         />
         <AsyncRoute
             path="/adverts"
             getComponent={() => import('./adverts/index').then(module => module.default)}
-            loading={() => <div>loading...</div>} />
+            loading={() => <Loader />} />
         <AsyncRoute
             path="/arts"
             getComponent={() => import('./arts/index').then(module => module.default)}
-            loading={() => <div>loading...</div>} />
+            loading={() => <Loader />} />
         <AsyncRoute
             path="/awards"
             getComponent={() => import('./awards/index').then(module => module.default)}
-            loading={() => <div>loading...</div>}
+            loading={() => <Loader />}
         />
     </Router>
 
