6162975e4fbc4b79ce7c77f3fd31f6dc38b61f78
Adil Shaikh <adil@invideo.io>
2020-12-20 20:14:23 +0530

    feat: experience ticker

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -2,8 +2,18 @@
 import React from "preact";
 import style from './style.css';
 import {aboutMeImg} from "../../utils/imgService";
+import {useEffect, useState} from "preact/hooks";
+import {getExperience} from "../../utils/calculationService";
 
 const Experience = () => {
+    const [exp, setExp] = useState({
+        years: 0,
+        months: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    });
     const data = [
         {place: 'ddb mudra', experience: '1.7 YEARS', designation: 'senior art director'},
         {place: 'leo burnett', experience: '3.8 YEARS', designation: 'art director'},
@@ -11,6 +21,16 @@ const Experience = () => {
         {place: 'percept art', experience: '1.0 YEARS', designation: 'freelance designer'}
     ];
 
+    useEffect(() => {
+        // 12 April 2012
+        // 12/04/2012
+        let id = setInterval(() => {
+            setExp(getExperience("04/12/2012"));
+        }, 1000)
+        return () => {
+            clearInterval(id)
+        }
+    });
     return <div class={style.parent}>
         <div class={style['image-wrapper']}>
             <img src={aboutMeImg("experience_shadow")} alt="yash-shadow" class={style['shadow-image']} />
@@ -21,17 +41,17 @@ const Experience = () => {
                 <div class={style['top-wrapper']}>
                     <div class={style.header}>experience</div>
                     <div class={style['time-parent']}>
-                        <div class={style.number} value="YEARS">07</div>
+                        <div class={style.number} value="YEARS">{exp.years}</div>
                         <div class={style.colon}>:</div>
-                        <div class={style.number} value="MONTHS">06</div>
+                        <div class={style.number} value="MONTHS">{exp.months}</div>
                         <div class={style.colon}>:</div>
-                        <div class={style.number} value="DAYS">06</div>
+                        <div class={style.number} value="DAYS">{exp.days}</div>
                         <div class={style.colon}>:</div>
-                        <div class={style.number} value="HOURS">13</div>
+                        <div class={style.number} value="HOURS">{exp.hours}</div>
                         <div class={style.colon}>:</div>
-                        <div class={style.number} value="MINUTES">24</div>
+                        <div class={style.number} value="MINUTES">{exp.minutes}</div>
                         <div class={style.colon}>:</div>
-                        <div class={style.number} value="SECONDS">55</div>
+                        <div class={style.number} value="SECONDS">{exp.seconds}</div>
                     </div>
                 </div>
                 <div class={style['middle-wrapper']}>
--- src/utils/calculationService.js
+++ src/utils/calculationService.js
@@ -3,5 +3,29 @@ export const getAge = date => {
     let check = new Date();
     let milliDay = 1000 * 60 * 60 * 24; // a day in milliseconds;
     let ageInDays = (check - birth) / milliDay;
-    return Math.floor(ageInDays / 365 );
+    return Math.floor(ageInDays / 365);
+}
+
+export const getExperience = startDate => {
+    let diff = Date.now() - (new Date(startDate)).getTime();
+    let seconds = Math.floor(diff / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+    let months = Math.floor(days / 30);
+    let years = Math.floor(days / 365);
+
+    seconds %= 60;
+    minutes %= 60;
+    hours %= 24;
+    days %= 30;
+    months %= 12;
+    return {
+        years,
+        months,
+        days,
+        hours,
+        minutes,
+        seconds
+    }
 }
