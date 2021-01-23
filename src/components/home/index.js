b080374b682a9fdc9f873789337256a0d845d941
Adil Shaikh <adil@invideo.io>
2021-01-23 18:19:01 +0530

    feat: responsive slot array

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -14,15 +14,15 @@ const Designation = () => <div class={style.designation}>
 
 const SlotMachine = (props) => {
     const slotsRef = createRef()
-    // const slots = [
-    //     "mbre  ",
-    //     "bout  ",
-    //     "wards ",
-    //     "dverts",
-    //     "rts   "
-    // ]
-
     const slots = [
+        "mbre--",
+        "bout--",
+        "wards-",
+        "dverts",
+        "rts---"
+    ]
+
+    const slots2 = [
         "-ambre-",
         "-about-",
         "-awards",
@@ -31,25 +31,40 @@ const SlotMachine = (props) => {
     ]
 
     useEffect(() => {
-        slots.forEach((item) => {
-            const temp = item.split("")
-            temp.forEach((it, index) => {
-                const element = document.createElement("span")
-                element.classList.add("cell");
-                element.innerText = it;
-                slotsRef.current.childNodes[index].appendChild(element)
+        const media = window.matchMedia("(max-width: 600px)")
+        const noOfColumns = Math.max(
+            ...media.matches
+                ? slots2.map(it => it.length)
+                : slots.map(it => it.length)
+        )
+
+        const slotContainer = slotsRef.current;
+        // creating columns
+        for (let i = 0; i < noOfColumns; ++i) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            slotContainer.appendChild(column)
+        }
+        (media.matches ? slots2 : slots)
+            .forEach((item) => {
+                const temp = item.split("")
+                temp.forEach((it, index) => {
+                    const element = document.createElement("span")
+                    element.classList.add("cell");
+                    element.innerText = it !== "-" ? it : "";
+                    slotContainer.childNodes[index].appendChild(element)
+                })
             })
-        })
     }, [])
 
     useEffect(() => {
         let cellHeight = document.getElementsByClassName("cell")[0].clientHeight;
         let children = slotsRef.current.childNodes
         for (let i = 0; i < children.length; ++i) {
-            const node = children[i];
+            const column = children[i];
             const delay = i * 0.2;
-            node.style.transitionDelay = `${delay}s`;
-            node.style.transform = `translateY(-${cellHeight * props.position}px)`;
+            column.style.transitionDelay = `${delay}s`;
+            column.style.transform = `translateY(-${cellHeight * props.position}px)`;
         }
     }, [props.position])
 
@@ -79,15 +94,7 @@ const SlotMachine = (props) => {
                 onClick={() => props.onPreviousClick()} />
             <div class={style["slot-container"]}>
                 <div class={style["a-text"]}>A</div>
-                <div class={style.slot} ref={slotsRef} onClick={() => redirect()}>
-                    <div class={style.column} />
-                    <div class={style.column} />
-                    <div class={style.column} />
-                    <div class={style.column} />
-                    <div class={style.column} />
-                    <div class={style.column} />
-                    <div class={style.column} />
-                </div>
+                <div class={style.slot} ref={slotsRef} onClick={() => redirect()} />
             </div>
             <img
                 style={{visibility: props.position < 4 ? "visible" : "hidden"}}
