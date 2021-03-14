0a3af123a03c6a1e13bfe248ee58d5934afbca52
Adil Shaikh <adil@invideo.io>
2021-03-14 21:22:17 +0530

    feat: on click of logo redirect to home

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,7 +1,8 @@
 import style from './style.css';
+import {route} from "preact-router";
 
 const Logo = (props) => {
-    return <div class={style["logo-parent"]}>
+    return <div class={style["logo-parent"]} onClick={() => route("home")}>
         <img class={style.logo} src={"assets/home_icon.svg"} />
         <span class={style["logo-text"]}>{props.title || "home"}</span>
     </div>
